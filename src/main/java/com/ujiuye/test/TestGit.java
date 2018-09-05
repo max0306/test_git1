0bb67e20c0c0703044390862b895dcaa08829dd3
max <1533980410@163.com>
2018-09-05 23:18:31 +0800

    第一次修改了TestGit类文件

--- src/main/java/com/ujiuye/test/TestGit.java
+++ src/main/java/com/ujiuye/test/TestGit.java
@@ -3,5 +3,6 @@ package com.ujiuye.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试一下Idea运用Git");
+        System.out.println("感觉还是很复杂啊");
     }
 }
